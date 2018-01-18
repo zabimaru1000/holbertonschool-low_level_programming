31c2fc8874ef6cf57d3b4de28d029d90847318b4
Brian Chew <bhchew@ucdavis.edu>
2018-01-18 06:29:22 +0000

    C program that prints precise string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
