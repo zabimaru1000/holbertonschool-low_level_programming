afe274c0f897e5ff7c7d8eb8b0c3de07bcdc1a10
Brian Chew <bhchew@ucdavis.edu>
2018-01-18 07:22:00 +0000

    C program that prints datatype size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,16 @@
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("Size of a char: %n byte(s)\n"), sizeof(char));
-printf("Size of an int: %n byte(s)\n"), sizeof(int));
-printf("Size of a long  int: %n byte(s)\n"), sizeof(long int));
-printf("Size of a long long int: %n byte(s)\n"), sizeof(long long int));
-printf("Size of a float: %n byte(s)\n"), sizeof(float));
+printf("Size of a char: %lu byte(s)\n"), sizeof(char));
+printf("Size of an int: %lu byte(s)\n"), sizeof(int));
+printf("Size of a long int: %lu byte(s)\n"), sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n"), sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n"), sizeof(float));
 
 return (0);
 }
