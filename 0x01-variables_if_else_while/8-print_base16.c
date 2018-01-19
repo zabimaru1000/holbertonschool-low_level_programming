a9ea31525fd117f0bdc286bcc1312b210e357389
272 <272@holbertonschool.com>
2018-01-19 02:37:38 +0000

    Program that prints binary base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers of binary base 16 starting from 0.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char alpha;
+
+	for (num = 0; num < 10; num++)
+		putchar('0' + num);
+
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+}
