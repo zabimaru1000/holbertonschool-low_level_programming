a8f473ddc2aa69a66677cf7b516502ac14cf8dd9
272 <272@holbertonschool.com>
2018-01-19 03:28:28 +0000

    Program that prints the numbers from 00 to 99.

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers of base 10 starting from 0 with specific syntax.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 100; num++)
+	{
+		putchar('0' + num/10);
+
+		if (num < 99)
+			putchar(',');
+		if (num < 99)
+			putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
