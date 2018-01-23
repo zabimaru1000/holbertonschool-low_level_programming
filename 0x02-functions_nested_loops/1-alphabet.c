3c526524fce5b056a7370b2c33150e9bd4813d91
Brian Chew <bhchew@ucdavis.edu>
2018-01-23 23:56:09 +0000

    C function that loops printing a character from a to z.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,6 @@
 void print_alphabet(void);
 
 /**
- * main - calls on the function print_alphabet
- * Return: 0 (Success)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - Loops a character starting with 'a' and prints until z.
  *
  * Uses for loop and _putchar function.
