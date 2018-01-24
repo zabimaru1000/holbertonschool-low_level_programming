7b7493b037a7283ba865cee29bafaf863d4a3518
Brian Chew <bhchew@ucdavis.edu>
2018-01-24 05:50:03 +0000

    Function that adds two numbers and stores the sum in an integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+int add(int, int);
+
+/**
+ * add - adds the sum of integers a & b and stores it in c.
+ *
+ * @a: 1st parameter that takes an integer to be added to b.
+ * @b: 2nd parameter that takes another integer to be added to a/
+ *
+ * Return: int c (returns sum of a & b to main)
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
