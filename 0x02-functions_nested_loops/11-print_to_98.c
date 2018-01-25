3025446f20c8b67f03888a95e26b1b56bfbd4e4d
Brian Chew <bhchew@ucdavis.edu>
2018-01-25 01:21:25 +0000

    Function that prints numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,16 +11,16 @@ void print_to_98(int n);
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n == 98)
+		printf("%d", n);
+
+	else if (n < 98)
 		for (n = n; n < 99; n++)
 		{
 			printf("%d", n);
 			if (n < 98)
 				printf(", ");
 		}
-
-	else if (n == 98)
-		printf("%d", n);
 
 	else
 		for (n = n; n > 99; n--)
