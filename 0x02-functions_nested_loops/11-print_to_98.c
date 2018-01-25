39f593f90be4c360ad4d89fc5bff5903d4db5ded
Brian Chew <bhchew@ucdavis.edu>
2018-01-25 01:10:03 +0000

    Function that prints any number to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "holberton.h"
+
+void print_to_98(int n);
+
+/**
+ * print_to_98 - Prints numbers up to 98 depending on where it starts.
+ * @n: Integer that is taken from main to be used in function.
+ * Description: Uses if else conditionals to print numbers.
+ *
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+		for (n = n; n < 99; n++)
+		{
+			printf("%d", n);
+			if (n < 98)
+				printf(", ");
+		}
+
+	else if (n == 98)
+		printf("%d", n);
+
+	else
+		for (n = n; n > 99; n--)
+		{
+			printf("%d", n);
+			if (n > 98)
+				printf(", ");
+		}
+
+	printf("\n");
+}
