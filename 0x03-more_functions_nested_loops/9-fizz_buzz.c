15c66b76696be1428910949e21fdb37760a3dc46
Brian Chew <bhchew@ucdavis.edu>
2018-01-26 06:37:16 +0000

    Fixed syntax

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int num;
 
-	for (num = 1; num < 101; num++)
+	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && num % 5 == 0)
+		if ((num % 3 == 0) && (num % 5 == 0))
 			printf("FizzBuzz ");
 		else if (num % 3 == 0)
 			printf("Fizz ");
