d29b3f205de686eb3fc1ead7a8d2f9d5e1a7db1e
Brian Chew <bhchew@ucdavis.edu>
2018-01-26 06:41:50 +0000

    No space after 100th iteration

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,15 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if ((num % 3 == 0) && (num % 5 == 0))
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (num % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (num % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", num);
+			printf("%d", num);
+		if (num < 100)
+			printf(" ");
 	}
 	printf("\n");
 
