21e64f5ae70fe2bf34247616d06663d53b296242
Brian Chew <bhchew@ucdavis.edu>
2018-01-26 04:53:07 +0000

    Checks if ASCII value is a digit

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _islower - Checks ASCII value if it is a digit.
+ *
+ * @c: Parameter to be evaluated by if else conditionals.
+ *
+ *
+ * Return: 1 or 0 (returns ASCII value in n to main)
+ */
+int _isdigit(int c)
+{
+
+	if (c == 48 || c <= 57)
+		return (1);
+
+	else
+		return (0);
+}
