119671939b271c859270bff8865703f814f04a59
Brian Chew <bhchew@ucdavis.edu>
2018-01-26 06:27:30 +0000

    Function that prints from 0 to 9 except 2 and 4

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,7 +9,7 @@ void print_most_numbers(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-		if ((num == 1 || num == 3) || (num >= 5))
+		if ((num == 0 || num == 1 || num == 3) || (num >= 5))
 			_putchar(num + '0');
 	_putchar('\n');
 }
