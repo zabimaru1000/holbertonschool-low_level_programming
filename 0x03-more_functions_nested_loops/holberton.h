abc86a64712e7bdb1b9e1c2ca86fc0b43e989516
Brian Chew <bhchew@ucdavis.edu>
2018-01-27 01:47:36 +0000

    Updated headers

--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -9,3 +9,8 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
+void one (int n);
+void two (int n);
+void three (int n);
+void four (int n);
