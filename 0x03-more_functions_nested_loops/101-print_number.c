cb7de3b41318935358486cee0453737344ee8b33
Brian Chew <bhchew@ucdavis.edu>
2018-01-27 01:46:56 +0000

    Function that prints an integer

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,64 @@
+#include "holberton.h"
+
+
+void print_number(int n)
+{
+	if (n >= -9 && n <= 9)
+		one (n);
+
+	else if ((n >= 10 && n <= 99) || (n <= -10 && n >= -99))
+		two (n);
+
+	else if (n >= 100 && n <= 999)
+		three (n);
+
+	else if (n >= 1000 && n <= 9999)
+		four (n);
+}
+
+
+void one (int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	_putchar('0' + n);
+}
+
+void two (int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	_putchar('0' + (n / 10));
+	_putchar('0' + (n % 10));
+}
+
+void three (int n)
+{
+	if (n < 0)
+        {
+                _putchar('-');
+                n = n * -1;
+        }
+	_putchar('0' + n / 100);
+	_putchar('0' + (n / 10) % 10);
+	_putchar('0' + n % 10);
+}
+
+void four (int n)
+{
+	if (n < 0)
+        {
+                _putchar('-');
+                n = n * -1;
+        }
+	_putchar('0' + n / 1000);
+	_putchar('0' + (n / 100) % 10);
+	_putchar('0' + (n / 10) % 10);
+	_putchar('0' + n % 10);
+}
