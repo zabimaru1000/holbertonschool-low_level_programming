d1d0fe4a561162bcfe747e1836b2341209fe2f96
Brian Chew <bhchew@ucdavis.edu>
2018-01-30 23:40:56 +0000

    Added function documentation

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
 
+/**
+ * reset_to_98 - update value to 98 via pointer
+ * @n: Pointer with memory address to n (function argument in main).
+ *
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
