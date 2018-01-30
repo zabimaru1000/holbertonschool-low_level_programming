89b27f2ced0facdaf55efeb7907c9833dd054ad7
Brian Chew <bhchew@ucdavis.edu>
2018-01-30 23:46:54 +0000

    Added documentation

--- 0x04-pointers_arrays_strings/1-swap.c
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * swap_int - Takes pointers to a and b and swaps their values.
+ * @a: Pointer with memory address to a.
+ * @b: Other pointer with memory address to b.
+ *
+ * Return: void
+ */
 void swap_int(int *a, int *b)
 {
 	*a = 42, *b = 98;
