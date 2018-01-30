28cfec05df92d8b2a3edcaefc4a55e7389cbc2cf
Brian Chew <bhchew@ucdavis.edu>
2018-01-30 23:41:22 +0000

    Fixed logic inside the loop

--- 0x04-pointers_arrays_strings/2-strlen.c
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,22 @@
 #include "holberton.h"
 
+/**
+ * _strlen - Returns an integer inside an array
+ * @s: pointer with address variable from str.
+ *
+ * Description: Loops an integer inside an array until it reaches NULL.
+ * Description (cont): Array contains the string "Holberton!"
+ *
+ * Return: init (An integer contained in an array)
+ */
 int _strlen(char *s)
 {
-	char init;
+	int init;
 
 	for (init = 0; s[init] != '\0'; init++)
-		return (init);
+	{
+	}
+
+	return (init);
+
 }
