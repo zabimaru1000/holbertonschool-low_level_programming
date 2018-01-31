830245171f013dee993943062602d58b36da0e3c
Brian Chew <bhchew@ucdavis.edu>
2018-01-31 02:52:52 +0000

    Changed hard coding to accomodate all integers

--- 0x04-pointers_arrays_strings/1-swap.c
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -5,7 +5,7 @@
  * @a: Pointer with memory address to a.
  * @b: Other pointer with memory address to b.
  * Description: Stores value in pointer to a to a placeholder variable.
- * Description (cont): Pointer a gets its value swapped, while b gets value from getA.
+ * Description (cont): a gets its value swapped, while b gets value from getA.
  *
  */
 void swap_int(int *a, int *b)
