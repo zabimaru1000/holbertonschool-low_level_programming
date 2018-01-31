f18513dddd25296ac8cfde664ea86a8340f9898c
Brian Chew <bhchew@ucdavis.edu>
2018-01-31 03:05:17 +0000

    Similar to task 3 except added an iterator

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * puts2 - Takes a pointer to a string and only prints every 2 characters.
+ * @str: Variable with datatype char that holds the string "0123456789"
+ *
+ * Description: Uses a for loop to print our array until NULL is reached.
+ * Description (cont): Adds another iteration by 1 to print every 2 characters.
+ *
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+
+}
