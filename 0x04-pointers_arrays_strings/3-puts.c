2ca41ea8311608f79b8a32d37cebbcafb4324916
Brian Chew <bhchew@ucdavis.edu>
2018-01-31 03:04:52 +0000

    Imitates puts()

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts - Imitates the std library puts function by printing a string.
+ *
+ * Description: Takes a loop and loops the array until NULL is reached.
+ * Description (cont): uses a custom putchar function to print the string.
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+
+}
