3477f638fb60c694b7d0338813a2f451a45f3553
Brian Chew <bhchew@ucdavis.edu>
2018-01-31 05:33:24 +0000

    Added parameter documentation for betty

--- 0x04-pointers_arrays_strings/3-puts.c
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -2,6 +2,7 @@
 
 /**
  * _puts - Imitates the std library puts function by printing a string.
+ * @str: Pointer that has the memory address of str.
  *
  * Description: Takes a loop and loops the array until NULL is reached.
  * Description (cont): uses a custom putchar function to print the string.
