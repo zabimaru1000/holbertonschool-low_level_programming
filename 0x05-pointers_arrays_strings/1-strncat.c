ca6b4b1e421b798a1ce0d7b0f1dd6c976c1ba3e8
Brian Chew <bhchew@ucdavis.edu>
2018-02-03 05:23:42 +0000

    Referred to strncpy

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -2,27 +2,46 @@
 
 int _strlen(char *s);
 
+/**
+ * _strncat - Concatenate two strings except uses n bytes from src.
+ * @dest: pointer to memory address that gets value of char dest
+ * @src: pointer to memory address that gets value of char src
+ * @n: integer that takes n bytes from string src.
+ *
+ * Description: Same concept as strcat except it is accounting for extra bytes.
+ *
+ * Return: dest (char type array depending on n bytes.
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-        int getA, getB;
+	int getA, getB;
 
-        getA = _strlen(dest);
+	getA = _strlen(dest);
 
-        for (getB = 0; src[getB] != '\0'; getB++)
-        {
-                dest[getA] = src[getB] + n;
-        }
+	for (getB = 0; src[getB] != '\0' && getB < n; getB++)
+	{
+		dest[getA] = src[getB];
+		getA++;
+	}
 
-        return (dest);
+	return (dest);
 }
 
+/**
+ * _strlen - Returns an integer inside an array
+ * @s: pointer with address variable from str.
+ *
+ * Description: Loops an integer inside an array until it reaches NULL.
+ *
+ * Return: init (An integer contained in an array)
+ */
 int _strlen(char *s)
 {
-        int init;
+	int init;
 
-        for (init = 0; s[init] != '\0'; init++)
-		;
+	for (init = 0; s[init] != '\0'; init++)
+	;
 
-        return (init);
+	return (init);
 
 }
