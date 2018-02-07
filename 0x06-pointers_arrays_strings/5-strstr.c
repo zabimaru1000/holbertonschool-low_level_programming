0f5310726bcee41d938282422281e970c12d3ca1
Brian Chew <bhchew@ucdavis.edu>
2018-02-07 07:33:11 +0000

    Fixed betty

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -17,14 +17,12 @@ char *_strstr(char *haystack, char *needle)
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (haystack [i+j] != needle[j])
-			    break;
-		}
+			if (haystack[i + j] != needle[j])
+				break;
 
 		if (needle[j] == '\0')
 			return (haystack + i);
-        }
+	}
 
 	return (0);
 }
