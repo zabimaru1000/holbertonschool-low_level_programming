381eda575a825c5a9acd6fa48fe6911996e5bce2
Brian Chew <bhchew@ucdavis.edu>
2018-02-07 07:30:21 +0000

    Missing edge cases

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,18 +1,30 @@
 #include "holberton.h"
+#include <stdio.h>
 
+/**
+ * _strstr - Locates a substring
+ * @haystack: Pointer to pointer s containing a char array.
+ * @needle: Pointer to pointer f containing a char array (usually substring)
+ *
+ * Description: Loops through needle within haystack to check substring.
+ *
+ * Return: haystack + i or 0 (Either a char value or NULL)
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
 	for (i = 0; haystack[i] != '\0'; i++)
+	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i + j] != needle[j])
-				break;
-	        }
+			if (haystack [i+j] != needle[j])
+			    break;
+		}
 
-	if (needle[j] == '\0')
-		return (needle + i);
+		if (needle[j] == '\0')
+			return (haystack + i);
+        }
 
 	return (0);
 }
