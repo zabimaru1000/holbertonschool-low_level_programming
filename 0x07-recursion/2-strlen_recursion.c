84e73edb079a911aa36cad4ab1244e6fc87ac19b
Brian Chew <bhchew@ucdavis.edu>
2018-02-07 19:15:01 +0000

    Base case now if s is not NULL

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -5,19 +5,16 @@
  * @s: Pointer to char array s.
  *
  * Description: Base case is if the string is not NULL.
- * Description (cont): Adds 1 to i every time recursion happens.
+ * Description (cont): Adds 1 to the function every time recursion happens.
  *
- * Return: i (Integer value containing number of char)
+ * Return: itself or 0 (0 if no string or recalls function and adds 1)
  */
 int _strlen_recursion(char *s)
 {
 	int i = 0;
 
 	if (s[i] != '\0')
-	{
-		i++;
-		_strlen_recursion(&s[i + 1]);
-	}
+		return (_strlen_recursion(&s[i + 1]) + 1);
 
-	return (i);
+	return (0);
 }
