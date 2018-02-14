e6f580f0c49e6c29b4c4210d0644b5b66df74624
Brian Chew <bhchew@ucdavis.edu>
2018-02-14 22:51:04 +0000

    PLD review

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -1,6 +1,8 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+int _strlen(char *s);
+
 /**
  * _strdup - Returns a char pointer to a new memory allocation.
  * @str: Char pointer to "Holberton"
@@ -10,16 +12,40 @@
 char *_strdup(char *str)
 {
 	unsigned int i;
-	char *cpy, *mem;
+	char *mem;
 
-	for (i = 0; str[i] != '\0'; i++)
-		cpy = str;
-
-	mem = malloc(sizeof(char) * i + 1);
-	mem = cpy;
-
-	if (mem == '\0' || str == '\0')
+	if (str == '\0')
 		return ('\0');
 
+	mem = malloc(sizeof(char) * _strlen(str) + 1);
+
+	if (mem == '\0')
+		return ('\0');
+
+	for (i = 0; str[i] != '\0'; i++)
+		mem[i] = str[i];
+
+	mem[i] = '\0';
+
 	return (mem);
+}
+
+/**
+ * _strlen - Returns an integer inside an array
+ * @s: pointer with address variable from str.
+ *
+ * Description: Loops an integer inside an array until it reaches NULL.
+ * Description (cont): Array contains the string "Holberton!"
+ *
+ * Return: init (An integer contained in an array)
+ */
+int _strlen(char *s)
+{
+	int init;
+
+	for (init = 0; s[init] != '\0'; init++)
+	{
+	}
+
+	return (init);
 }
