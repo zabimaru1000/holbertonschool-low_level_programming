14e9cc2dbc758ce85b0e93e25fae3688e138a123
Brian Chew <bhchew@ucdavis.edu>
2018-02-14 04:46:05 +0000

    1st commit

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a char pointer to a new memory allocation.
+ * @str: Char pointer to "Holberton"
+ *
+ * Return: mem (Pointer to allocated memory in new_array set by size.)
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *cpy, *mem;
+
+	if (str == '\0')
+		return ('\0');
+
+	for (i = 0; str[i] != '\0'; i++)
+		cpy = str;
+
+	mem = malloc(sizeof(char) * i + 1);
+
+	mem = cpy;
+
+
+	return (mem);
+}
