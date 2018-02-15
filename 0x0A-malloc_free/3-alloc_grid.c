775e49ad0ccd035cff1a91c2b26378c2d14399f1
Brian Chew <bhchew@ucdavis.edu>
2018-02-15 05:44:52 +0000

    Added everything freed if malloc fails

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -25,7 +25,13 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 
 		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+
+			free(grid);
 			return ('\0');
+		}
 
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
