20a14fef6c9d483fd2b268ed974344decc794cf8
Brian Chew <bhchew@ucdavis.edu>
2018-02-15 05:41:04 +0000

    Test

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -1,14 +1,21 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+ * alloc_grid - Returns pointer to 2D int array.
+ * @width: Size of row in 2D array.
+ * @height: Size of column in 2D array.
+ *
+ * Return: grid (Initalized value in array that's on the allocated memory)
+ */
 int **alloc_grid(int width, int height)
 {
 	int i, j, **grid;
 
+	grid = malloc(sizeof(int *) * height);
+
 	if (width <= 0 || height <= 0)
 		return ('\0');
-
-	grid = malloc(sizeof(int *) * height);
 
 	if (grid == NULL)
 		return ('\0');
@@ -16,10 +23,12 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
+
 		if (grid[i] == NULL)
 			return ('\0');
+
 		for (j = 0; j < width; j++)
-			grid[height][width] = 0;
+			grid[i][j] = 0;
 	}
 
 	return (grid);
