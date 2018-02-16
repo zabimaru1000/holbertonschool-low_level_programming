3239eec1b53eaefdc9d9eb7c72e13ca51bc77158
Brian Chew <bhchew@ucdavis.edu>
2018-02-16 05:39:18 +0000

    Added memset

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+char *_memset(char *s, char b, unsigned int n);
+
 /**
  * _calloc - Allocates memory for an array. Imitates the calloc function.
  * @nmemb: Unsigned int type representing elements of an array.
  * @size: Same data type representing number of bytes in memory.
  *
- *
  * Return: mem (Pointer to a certain amount of space in memory)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *mem;
+	char *mem;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,6 +22,19 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (mem == 0)
 		return (NULL);
 
+	if (mem != NULL)
+		_memset(mem, 0, size * nmemb);
+
 	return (mem);
 
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
 }
