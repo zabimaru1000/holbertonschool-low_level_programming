361e07ea5ef17a0189dac12b96214f7666810481
Brian Chew <bhchew@ucdavis.edu>
2018-02-16 07:04:37 +0000

    Fixed malloc

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	mem = malloc(sizeof(char) * size + nmemb);
+	mem = malloc(sizeof(char) * size * nmemb);
 
 	if (mem == 0)
 		return (NULL);
