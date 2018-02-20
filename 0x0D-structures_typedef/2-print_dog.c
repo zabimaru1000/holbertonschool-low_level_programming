6accc564ede10c1305b71540402525057b34edf4
Brian Chew <bhchew@ucdavis.edu>
2018-02-20 06:28:03 +0000

    Checks float w/ 0

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d == 0)
 		return;
 
-	if (d->name == 0)
+	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Age: %f\n", d->age);
 
-	if (d->owner == 0)
+	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
