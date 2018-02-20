e3db9e5ed2d8a559be6a0d7686564d3daed1d88a
Brian Chew <bhchew@ucdavis.edu>
2018-02-20 06:12:38 +0000

    Added NULL edge cases

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -8,10 +8,21 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		printf("\n");
+	if (d == 0)
+		return;
 
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->name == 0)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age == 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == 0)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
