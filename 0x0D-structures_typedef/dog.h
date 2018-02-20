4fbf65bee79931512d407308cdd63b1f24f89bea
Brian Chew <bhchew@ucdavis.edu>
2018-02-20 05:27:23 +0000

    fixed betty

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct containing char pointer and float types.
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
 	char *owner;
 };
