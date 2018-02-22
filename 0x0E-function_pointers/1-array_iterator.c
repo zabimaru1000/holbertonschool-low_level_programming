6173e5675bbb0c2fb1080cfcd6dc8631eefd2cce
Brian Chew <bhchew@ucdavis.edu>
2018-02-22 02:41:48 +0000

    1st commit

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Points to two functions & derefrences them for each element
+ * @array: Int pointer to an array of numbers.
+ * @size: Size of array.
+ * @action: Pointer to function print_elem or print_elem_hex.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action != NULL)
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
+}
