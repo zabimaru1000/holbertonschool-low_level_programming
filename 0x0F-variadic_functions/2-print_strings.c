55582bb2911e3791cc35f3c82da062052b18dc86
Brian Chew <bhchew@ucdavis.edu>
2018-02-24 23:30:32 +0000

    Fixed edge cases for nil

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -2,6 +2,12 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+
+/**
+ * print_strings - Prints strings followed by newline.
+ * @separator: A char pointer to a constant ", "
+ * @n: Constant int set to one number.
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
@@ -13,21 +19,27 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	if (separator == NULL)
 		separator = "";
 
+	if (va_arg(list, char *) == NULL)
+	{
+		printf("(nil)\n");
+		return;
+	}
+
 	for (i = 0; i < n; i++)
 	{
 		c = va_arg(list, char *);
 
-		if (c == NULL)
-			return;
-
-		else if (i < n - 1)
+		if (i < n - 1)
 		{
+			if (c == NULL)
+			{
+				printf("(nil)\n");
+				return;
+			}
 			printf("%s%s", c, separator);
 		}
 		else
-		{
 			printf("%s", c);
-		}
 	}
 
 	printf("\n");
