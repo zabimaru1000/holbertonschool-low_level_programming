571b606e1d02c9c7f7a1c685b3499e315c16379e
Brian Chew <bhchew@ucdavis.edu>
2018-02-24 20:25:05 +0000

    Added if condition to check separator

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -2,23 +2,30 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+/**
+ * print_numbers - Prints numbers followed by new line.
+ * @separator: A char pointer to a constant ", "
+ * @n: Constant int set to one number.
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
-	unsigned int i, b;
+	unsigned int i;
 
 	va_start(list, n);
 
 	if (separator == NULL)
 		return;
 
-	va_arg(list, b);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d%s", b, separator);
+		if (i < n - 1)
+			printf("%d%s", va_arg(list, unsigned int), separator);
+		else
+			printf("%d", va_arg(list, unsigned int));
 	}
 
-        printf("\n");
+	printf("\n");
 	va_end(list);
 
 }
