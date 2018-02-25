174cea3fe155f16a6a39df755f06d7394adb28c2
Brian Chew <bhchew@ucdavis.edu>
2018-02-25 07:15:37 +0000

    changed structs and edge cases

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
 
 void pr_char(va_list list);
@@ -7,43 +5,85 @@ void pr_int(va_list list);
 void pr_float(va_list list);
 void pr_str(va_list list);
 
+/**
+ * print_all - Function that prints anything in the arguments.
+ * @format: Format string.
+ */
 void print_all(const char * const format, ...)
 {
-	int i;
+	int i, j;
+	char *separator;
 	va_list list;
-
-	va_start(list, *format);
-
-        p_t pr_format[] = {
+	p_t pre[] = {
 		{"c", pr_char},
 		{"i", pr_int},
 		{"f", pr_float},
 		{"s", pr_str},
 		{NULL, NULL}
 	};
+
+	va_start(list, format);
+
 	i = 0;
-
-	while (format[i] != NULL)
+	j = 0;
+	separator = "";
+	while (format != NULL && format[i] != '\0')
 	{
-		pr_format[i].f(list);
+		while (pre[j].fp != NULL)
+		{
+			if (format[i] == pre[j].fp[0])
+			{
+				printf("%s", separator);
+				pre[j].f(list);
+				separator = ", ";
+			}
+			j++;
+		}
 		i++;
+		j = 0;
 	}
-
+	printf("\n");
+	va_end(list);
 }
 
+/**
+ * pr_char - Prints a char.
+ * @list: Argument list.
+ */
 void pr_char(va_list list)
 {
-	printf("%c, ", va_arg(list, int));
+	printf("%c", va_arg(list, int));
 }
+/**
+ * pr_int - Prints an int.
+ * @list: List of argument
+ */
 void pr_int(va_list list)
 {
-	printf("%d, ", va_arg(list, int));
+	printf("%d", va_arg(list, int));
 }
+/**
+ * pr_float - Prints a float.
+ * @list: List of argument.
+ */
 void pr_float(va_list list)
 {
-	printf("%f, ", va_arg(list, double));
+	printf("%f", va_arg(list, double));
 }
+
+/**
+ * pr_str - Prints a string.
+ * @list: List of arguments.
+ */
 void pr_str(va_list list)
 {
-	printf("%s", va_arg(list, char *));
+	char *str;
+
+	str = va_arg(list, char *);
+	if (str != NULL)
+	{
+		printf("%s", str);
+		return;
+	}
+	printf("(nil)");
 }
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,6 +1,17 @@
 #ifndef VARIADIC_FUNC
 #define VARIADIC_FUNC
 
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct pre
+{
+	char *fp;
+	void (*f)(va_list);
+
+} p_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
