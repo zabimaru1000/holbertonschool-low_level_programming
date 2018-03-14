7b4a3cdb4ec333d5dcaa5c57c432502558b00cea
Brian Chew <bhchew@ucdavis.edu>
2018-03-14 05:23:23 +0000

    Fixed betty

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	bytes = sizeof(text_content);
 
-	if (filename == NULL )
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content == NULL)
