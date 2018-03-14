5b6f7ac005a4e294466bf73d78df1e752c27252d
Brian Chew <bhchew@ucdavis.edu>
2018-03-14 23:33:34 +0000

    Changed sizeof to strlen, switched NULL check

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
 
+int _strlen(char *s);
+
 /**
  * create_file - Creates file with NULL terminated string in 2nd arg
  * @filename: Char pointer to the files name.
@@ -8,27 +10,45 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, text;
-	ssize_t bytes;
+	int fd, text, bytes;
 
-	bytes = sizeof(text_content) - 3;
+	if (text_content == NULL)
+		text_content = "";
 
-	if (filename != NULL)
+	bytes = _strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	text = write(fd, text_content, bytes);
+	if (text == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
+
+/**
+ * _strlen - Returns an integer inside an array
+ * @s: pointer with address variable from str.
+ *
+ * Description: Loops an integer inside an array until it reaches NULL.
+ * Description (cont): Array contains the string "Holberton!"
+ *
+ * Return: init (An integer contained in an array)
+ */
+int _strlen(char *s)
+{
+	int init;
+
+	for (init = 0; s[init] != '\0'; init++)
 	{
-
-		if (text_content == NULL)
-			text_content = "";
-
-		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-		text = write(fd, text_content, bytes);
-
-		if (text < 0)
-			return (-1);
-
-		close(fd);
-		return (1);
 	}
 
-	return (-1);
+	return (init);
+
 }
