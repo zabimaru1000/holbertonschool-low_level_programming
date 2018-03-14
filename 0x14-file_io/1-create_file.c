e5a6e4a1df0aeee4a9c56ab13f31d6844f7c5f4c
Brian Chew <bhchew@ucdavis.edu>
2018-03-14 05:36:46 +0000

    Fixed betty

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		text = write(fd, text_content, bytes);
 
 		if (text < 0)
-			return(-1);
+			return (-1);
 
 		close(fd);
 		return (1);
