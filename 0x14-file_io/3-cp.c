e51cfb1ef8caca8ef87e23206dd06ba2277f5c0a
Brian Chew <bhchew@ucdavis.edu>
2018-03-14 21:46:21 +0000

    Fied open and write and STDERR

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -9,10 +9,8 @@
  */
 int main(int ac, char **av)
 {
-	int fd;
-	ssize_t bytes;
-
-	bytes = sizeof(av[1]);
+	int fd, fd2, get_read, get_write;
+	char *buffer[1024];
 
 	if (ac != 3)
 	{
@@ -20,17 +18,51 @@ int main(int ac, char **av)
 		exit(97);
 	}
 
-	if (av[1] == NULL)
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 
-	fd = open(av[1], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	write(fd, av[2], bytes);
+	fd2 = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 
+	get_read = 1024;
+	while (get_read == 1024)
+	{
+		get_read = read(fd, buffer, 1024);
+		if (get_read == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		get_write = write(fd2, buffer, get_read);
+		if (get_write == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 	close(fd);
+
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	close(fd2);
 
 	return (0);
 }
