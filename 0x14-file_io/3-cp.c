e0e9abe6ec71a8f69d0fec5b63df18db72bf27db
Brian Chew <bhchew@ucdavis.edu>
2018-03-18 00:19:15 +0000

    Changed oflag order

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -19,19 +19,22 @@ int main(int ac, char **av)
 	}
 
 	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+        if (fd == -1)
+        {
+                dprintf(2, "Sup: Can't read from file %s\n", av[1]);
+                exit(98);
+        }
 
-	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
 
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
+
 
 	get_read = 1024;
 	while (get_read == 1024)
