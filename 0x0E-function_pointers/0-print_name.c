dccaa64bd004310eb092811d274e7f64102c73ea
Brian Chew <bhchew@ucdavis.edu>
2018-03-31 02:10:23 +0000

    1st commit

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Points to a different function and dereferences.
+ * @name: Pointer to name in argument.
+ * @f: Pointer to function returning void with a ptr argument.
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
