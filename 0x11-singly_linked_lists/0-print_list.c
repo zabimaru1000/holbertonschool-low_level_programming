434b7d1482b73678fb9f1b9de9b44fefe933fb83
Brian Chew <bhchew@ucdavis.edu>
2018-04-08 06:08:09 +0000

    1st commit

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints elements of a linked list
+ * @h: Pointer to struct
+ *
+ * Return: 0 if NULL, i if success (elements).
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 1;
+
+	if (h == NULL)
+		return (0);
+
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+
+	i++;
+	print_list(h->next);
+
+	return (i);
+}
