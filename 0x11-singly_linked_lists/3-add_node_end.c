ce60c6ec2c56a2164304695af814b8d006afd42f
Brian Chew <bhchew@ucdavis.edu>
2018-04-10 06:39:21 +0000

    Declared a new pointer to struct

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -1,9 +1,16 @@
 #include "lists.h"
 
+/**
+ * add_node_end - Adds nodes at the end of linked list.
+ * @head: Pointer to beginning of address of struct.
+ * @str: Char pointer to str.
+ *
+ * Return: new_last on success, NULL if failed.
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int i;
-	list_t *new_last;
+	int i, j;
+	list_t *new_last, *get_node;
 
 	for (i = 0; str[i] != '\0'; i++)
 	;
@@ -14,14 +21,21 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		new_last->str = strdup(str);
 		new_last->len = i;
+		new_last->next = NULL;
+		get_node = *head;
+
 		if (*head == NULL)
 		{
 			*head = new_last;
-			new_last->next = *head;
+			return (new_last);
 		}
-		new_last->next = *head;
-		*head = new_last;
+
+		for (j = 0; get_node->next != NULL; j++)
+			get_node = get_node->next;
+
+		get_node->next = new_last;
 		return (new_last);
+
 	}
 
 	return (NULL);
