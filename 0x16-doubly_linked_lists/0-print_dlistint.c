e56adecaaf417cc8c3090cf77a203c9deae34356
Brian Chew <bhchew@ucdavis.edu>
2018-04-15 00:32:52 +0000

    Added betty

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints all elements of a doubly linked list that has an int
+ * @h: pointer to struct containing the 1st element of list.
+ * Return: 0 if h is NULL, or returns number of elements in list.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
