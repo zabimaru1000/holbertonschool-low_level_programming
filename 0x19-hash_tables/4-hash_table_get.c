053a436e39fd74378199854cdb95a75bc125e651
Brian Chew <bhchew@ucdavis.edu>
2018-05-19 06:40:42 +0000

    Returns value inside loop instead, place strcmp value inside loop

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -9,23 +9,28 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *dict;
-	unsigned long int index = 0;
+	unsigned long int index;
 	char *key_value;
 	int keycmp;
 
 	if (key == NULL || ht == NULL)
-		return NULL;
+		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
 	dict = ht->array[index];
-	keycmp = strcmp(key, dict->key);
+	key_value = dict->value;
 
 	while (dict != NULL)
+	{
+		keycmp = strcmp(key, dict->key);
+		if (keycmp == 0)
+			return (key_value);
 		dict = dict->next;
+	}
+
 
 	if (keycmp != 0 || dict != NULL || dict->key == NULL)
 		return (NULL);
 
-	key_value = dict->value;
-	return (key_value);
+	return (NULL);
 }
