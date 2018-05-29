f7cfe923298f438d151ce6da1e5562ba926ce92e
Brian Chew <bhchew@ucdavis.edu>
2018-05-29 08:09:27 +0000

    1st commit

---
+++ 0x1A-sorting_alogrithms/2-selection_sort.c
@@ -0,0 +1,34 @@
+#include  "sort.h"
+
+/**
+ * selection_sort - Sorts array of integers by ascending order
+ * @array: Pointer to array of integers
+ * @size: The size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j, index;
+	int temp;
+
+	/*Loop until end of array is reached.*/
+	while (i < size - 1)
+	{
+		index = i;
+		j = i + 1;
+		while (j < size)
+		{
+			if (array[index] > array[j])
+				index = j;
+			j++;
+		}
+
+		temp = array[i];
+		array[i] = array[index];
+		array[index] = temp;
+
+		if (index != i)
+			print_array(array, size);
+
+		i++;
+	}
+}
