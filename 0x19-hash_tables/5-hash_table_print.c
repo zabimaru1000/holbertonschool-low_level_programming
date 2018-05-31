259f41ef6fac90de2b871b5013563bf7b6b75ac7
Brian Chew <bhchew@ucdavis.edu>
2018-05-31 14:05:08 +0000

    1st commit on github, revised on git

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: Pointer to hash table containing size and array
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *print_ht;
+	unsigned long int size = ht->size;
+	unsigned int i = 0;
+	int comma_ctr;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	while (i < size)
+	{
+		print_ht = ht->array[i];
+		while (print_ht)
+		{
+			if (comma_ctr == 1)
+				printf(", ");
+			printf("'%s': '%s'", print_ht->key, print_ht->value);
+
+			/*Set comma counter to 1 and iterate next node*/
+			comma_ctr = 1;
+			print_ht = print_ht->next;
+		}
+
+		i++;
+	}
+
+	/*Sandwich print statements between loop to make {dict}*/
+	printf("}\n");
+}
